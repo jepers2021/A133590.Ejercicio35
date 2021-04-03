8236b8d432c517ad1ce18211230cbe0626082d97
Jorge <jepers2021@gmail.com>
2021-04-03 09:40:43 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace A133590.Ejercicio35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 35");
+            Console.WriteLine("Programa que recibe ingresos hasta que el mismo sea un número válido.");
+            double ingreso;
+            Console.Write("Por favor, ingrese un número: ");
+            while( !Double.TryParse(Console.ReadLine(), out ingreso) )
+            {
+                Console.Write("Ingreso inválido, intente nuevamente: ");
+            }
+
+            Console.WriteLine("Presione cualquier tecla para continuar..");
+            Console.ReadKey();
+        }
+    }
+}
